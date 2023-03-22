0bc1484e3742492130dbe266193da53a97d83c5f
CristixCudla <127952703+CristixCudla@users.noreply.github.com>
2023-03-22 11:10:24 +0200

    Add files via upload

---
+++ piu tema/ConsoleApp1/ConsoleApp1/Model.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Model
+    {
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+        private int idModel;
+        public string nume_model;
+        public string tip;
+
+
+        public Model()
+        {
+            nume_model = tip = string.Empty;
+            idModel = 0;
+        }
+        public Model(int idModel, string Nume, string Tip)
+        {
+            this.idModel = idModel;
+            this.nume_model = Nume;
+            this.tip = Tip;
+
+
+
+        }
+
+        public Model(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            idModel = Convert.ToInt32(dateFisier[0]);
+            nume_model = dateFisier[1];
+            tip = dateFisier[2];
+
+        }
+
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectProdusPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                idModel,
+                nume_model,
+                tip
+              );
+
+            return obiectProdusPentruFisier;
+        }
+
+        public void SetIdModel(int idModel)
+        {
+            this.idModel = idModel;
+        }
+
+        public int GetIdModel()
+        {
+            return idModel;
+        }
+
+
+        public string Get_nume()
+        {
+            return nume_model;
+        }
+
+        public string Get_tip()
+        {
+            return tip;
+        }
+
+    }
+}
---
+++ piu tema/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+
+
+
+using ConsoleApp1;
+
+
+namespace GSM_bun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("C. Citire produs de la tastatura");
+                Console.WriteLine("D. Citire furnizor de la tastatura");
+                Console.WriteLine("A. Cautare produs dupa nume");
+                Console.WriteLine("V. Cautare furnizor dupa nume");
+                Console.WriteLine("F. Afisare produselor din fisier");
+                Console.WriteLine("W. Afisare furnizorilor din fisier");
+                Console.WriteLine("S. Salvare produs in fisier");
+                Console.WriteLine("M. Salvare furnizor in fisier");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+               
